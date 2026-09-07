e170cc05ecc7063b172d459d5439a5a56bb3bfa3
sadiaamir-tech <sadia.amir2006@gmail.com>
2026-09-07 20:23:26 +0500

    commit

---
+++ 69. Sqrt(x).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+
+        int ans = 0;
+
+        for (long long i = 1; i <= x; i++) {
+            if (i * i <= x)
+                ans = i;
+            else
+                break;
+        }
+
+        return ans;
+    }
+};
